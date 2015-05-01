381409975147edc72c7dec188d8d41157e342f28
Phil Hannent <phil@elmstonesystems.co.uk>
2015-05-01 13:48:37 +0100

    Refactor the bad word check so that it can be tested, add a bunch of tests

---
+++ test/badWords.js
@@ -0,0 +1,42 @@
+// --------------------------------------------------------------------------------------------------------------------
+//
+// test/badWords.js : Test the generate function.
+//
+// Author           : Phil Hannent
+// Web              : https://www.hannent.uk
+// Email            : <phil@hannent.uk>
+//
+// Copyright (c)    : 2015 Phil Hannent
+// License          : http://opensource.org/licenses/MIT
+//
+// --------------------------------------------------------------------------------------------------------------------
+
+// npm
+var test = require("tape");
+
+// local
+var cc = require('../coupon-code');
+
+// --------------------------------------------------------------------------------------------------------------------
+
+var badWordsList = ('SHPX PHAG JNAX JNAT CVFF PBPX FUVG GJNG GVGF SNEG URYY ZHSS QVPX XABO ' +
+    'NEFR FUNT GBFF FYHG GHEQ FYNT PENC CBBC OHGG SRPX OBBO WVFZ WVMM CUNG')
+        .replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})
+        .split(" ");
+
+test("Checking that bad words can be found", function (t) {
+    var i;
+    // check each rude word is locatable in the string
+    for( i = 0; i < badWordsList.length; i++ ) {
+        t.ok( cc.checkForBadWords(badWordsList[i] + 'YYYYZZZZ'), 'Bad word found at start of code');
+        t.ok( cc.checkForBadWords('XXXX' + badWordsList[i] + 'ZZZZ'), 'Bad word found in middle of code');
+        t.ok( cc.checkForBadWords('YYYYZZZZ' + badWordsList[i]), 'Bad word found at end of code');
+    }
+
+    t.ok(cc.checkForBadWords('XXXX' + badWordsList[0].toLowerCase() + 'YYYYZZZZ'), 'Lower case bad word found in code');
+    t.ok(!cc.checkForBadWords('XXXXYYYYZZZZ'), 'No bad words found in code');
+
+    t.end();
+});
+
+// --------------------------------------------------------------------------------------------------------------------
